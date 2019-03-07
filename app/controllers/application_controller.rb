5a6112f68084f027f356eddb87d5b41b06bf7f77
tientt-glose <tiennguyenbka198@gmail.com>
2019-03-07 11:22:55 +0700

    refs rOrTuts #chapter_3 Super Final (i18n done, coding convention done)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,19 @@
 class ApplicationController < ActionController::Base
-	 protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
+  before_action :set_locale
 
+  private
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
+  end
+
+  # protect_from_forgery with: :exception
+  # chapter_3 Finish chapter_3
+  # chapter_3 Finish chapter_3
   def hello
     render html: "hello, world!"
   end
